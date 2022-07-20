7633b7da60876965ee2e2aacc49a6d36f6a91197
kotobukid <kotobuki@daqua.jp>
2022-07-20 13:54:17 +0900

    ミス修正、コードの整理

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
     svg(width="400" height="320" viewBox="0 0 400 320")
+        rect(x="0" y="0" width="400" height="320" fill="black")
         line(:x1="line.x1" :y1="line.y1" :x2="line.x2" :y2="line.y2" :stroke-width="line.width" stroke="brown" fill="none")
-        path(:d="polygon_points_text" fill="none" stroke="blue" stroke-width="1")
+        path(:d="polygon_points_text" fill="none" stroke="white" stroke-width="1")
         circle.p1(:cx="polygon.points[0].x" :cy="polygon.points[0].y" r="3" fill="red")
         circle.p2(:cx="polygon.points[1].x" :cy="polygon.points[1].y" r="3" fill="blue")
         circle.p3(:cx="polygon.points[2].x" :cy="polygon.points[2].y" r="3" fill="green")
@@ -10,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 import _ from 'lodash';
 
 declare type LineSource = {
@@ -33,29 +34,29 @@ const line2nGon = (line: LineSource): nGon => {
     const rad90: number = Math.PI / 2;
     const theta: number = Math.atan2(line.y2 - line.y1, line.x2 - line.x1);
 
+    const p0: Point2D = {
+        x: line.x2 + Math.cos(theta + rad90) * distance,
+        y: line.y2 + Math.sin(theta + rad90) * distance
+    };
     const p1: Point2D = {
-        x: line.x2 + Math.cos(rad90 + theta) * distance,
-        y: line.y2 + Math.sin(rad90 + theta) * distance
+        x: line.x1 + Math.cos(theta + rad90) * distance,
+        y: line.y1 + Math.sin(theta + rad90) * distance
     };
     const p2: Point2D = {
-        x: line.x1 + Math.cos(rad90 + theta) * distance,
-        y: line.y1 + Math.sin(rad90 + theta) * distance
+        x: line.x1 + Math.cos(theta - rad90) * distance,
+        y: line.y1 + Math.sin(theta - rad90) * distance
     };
     const p3: Point2D = {
-        x: line.x1 + Math.cos(-rad90 + theta) * distance,
-        y: line.y1 + Math.sin(-rad90 + theta) * distance
-    };
-    const p4: Point2D = {
-        x: line.x2 + Math.cos(-rad90 * theta) * distance,
-        y: line.y2 + Math.sin(-rad90 * theta) * distance
+        x: line.x2 + Math.cos(theta - rad90) * distance,
+        y: line.y2 + Math.sin(theta - rad90) * distance
     };
 
     return {
         points: [
+            p0,
             p1,
             p2,
             p3,
-            p4,
         ]
     };
 }
@@ -65,8 +66,8 @@ export default class HelloWorld extends Vue {
     line: LineSource = {
         x1: 10,
         y1: 10,
-        x2: 350,
-        y2: 305,
+        x2: 300,
+        y2: 275,
         width: 8.5 * 2
     }
 
@@ -75,10 +76,11 @@ export default class HelloWorld extends Vue {
     }
 
     get polygon_points_text(): string {
-        return _.map<Point2D, string>(this.polygon.points, (p: Point2D, index: number): string => {
+        const points: Point2D[] = this.polygon.points;
+        return _.map<Point2D, string>(points, (p: Point2D, index: number): string => {
             if (index === 0) {
                 return `M ${p.x},${p.y}`;
-            } else if (index === this.polygon.points.length - 1) {
+            } else if (index === points.length - 1) {
                 return `L ${p.x},${p.y} Z`;
             } else {
                 return `L ${p.x},${p.y}`;
